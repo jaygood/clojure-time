48bde26f98f78fd3cbbf966fd3f6febe543b38ca
Jon <haygoodjon@gmail.com>
2017-08-14 18:43:28 -0400

    Convert node to lein package

--- src/clojure_time/core.cljs
+++ src/clojure_time/core.cljs
@@ -4,17 +4,15 @@
             [clojure-time.config :refer [config]]
             [clojure-time.app :refer [app]]))
 
-(aset js/window "clojin_time"
-      (fn [c] (let [conf (js->clj c :keywordize-keys true)
-                    app-state (reagent/atom {:options {}
-                                             :templates (:templates conf)
-                                             :current (:name (nth (:templates conf) 0))})]
-                ; FIXME call in didMount or somethin
-                (switch-template! app-state (:current @app-state))
-                (reagent/render-component [app (or conf config) app-state] (.getElementById js/document "app")))))
+(defn clojin-time [c] (let [conf (js->clj c :keywordize-keys true)
+                            app-state (reagent/atom {:options {}
+                                                     :templates (:templates conf)
+                                                     :current (:name (nth (:templates conf) 0))})]
+                        ; FIXME call in didMount or somethin
+                        (switch-template! app-state (:current @app-state))
+                        (reagent/render-component [app (or conf config) app-state] (.getElementById js/document "app"))))
 
-; FIXME
-;(.clojin_time js/window config)
+(aset js/window "clojin_time" clojin-time)
 
 (defn on-js-reload [])
 ;; optionally touch your app-state to force rerendering depending on
